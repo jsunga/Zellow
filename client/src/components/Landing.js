b6e533bca105c8f1f4c258ac8c3f56c09acb674e
jsunga <jawyn.sunga@gmail.com>
2019-07-22 00:40:33 -0700

    add navbar with responsive design

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,9 +4,6 @@ export default class Landing extends Component {
     render() {
         return (
             <div>
-                <h1>Zellow</h1>
-                <h2>Reimagine home</h2>
-                <p>Skip the hassle and sell your home directly to Zillow. No prep work or showings required. Available in select areas with more coming soon.</p>
             </div>
         )
     }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import home from '../assets/home.png'
+import '../styles/navbar.scss'
 
 export default class Navbar extends Component {
+
     render() {
         return (
-            <div>
-                
+            <div className='navbar'>
+                <section className='left-container'>
+                    <ul className='left-wrapper'>
+                        <Link to='/for_rent' className='link'><li>Rent</li></Link>
+                        <Link to='/homes' className='link'><li>Buy</li></Link>
+                        <li><form><input placeholder='Enter city or ZIP code' /></form></li>
+                    </ul>
+                </section>
+                <section className='middle-container'>
+                    <Link to='/' className='link'>
+                        <h1><img src={home} height='36px' width='36px' alt='logo' />Zellow</h1>
+                    </Link>
+                </section>
+                <section className='right-container'>
+                    <ul className='right-wrapper'>
+                        <Link to='/list_your_rental' className='link'><li>List your rental</li></Link>
+                        <Link to='/sell' className='link'><li>Sell</li></Link>
+                        <Link to='/sign_in' className='link'><li>Sign in or Join</li></Link>
+                    </ul>
+                </section>
+                <div className='mobile'>
+                    <Link to='/' className='link'><h1><img src={home} height='36px' width='36px' alt='logo' />Zellow</h1></Link>
+                    <ul>
+                        <li><form><input placeholder='Enter city or ZIP code' /></form></li>
+                        <Link to='/sign_in' className='link'><li>Sign in or Join</li></Link>
+                        <Link to='/for_rent' className='link'><li>Rent</li></Link>
+                        <Link to='/homes' className='link'><li>Buy</li></Link>
+                        <Link to='/list_your_rental' className='link'><li>List your rental</li></Link>
+                        <Link to='/sell' className='link'><li>Sell</li></Link>
+                    </ul>
+                </div>
             </div>
         )
     }
+
 }
