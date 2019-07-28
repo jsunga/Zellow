8db2242640464b397398108ae3a2d23b7affe13e
jsunga <jawyn.sunga@gmail.com>
2019-07-27 23:16:57 -0700

    add footer image

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import home from '../assets/home.png'
+import facebook from '../assets/facebook.png'
+import twitter from '../assets/twitter.png'
 import '../styles/landing.scss'
 
 export default class Landing extends Component {
@@ -27,6 +30,31 @@ export default class Landing extends Component {
                                 
                             </section>
                         </div>
+                    </div>
+                </div>
+                <div className='footer'>
+                    <main>
+                        <ul>
+                            <li>About</li>
+                            <li>Estimates</li>
+                            <li>Research</li>
+                            <li>Careers</li>
+                            <li>Help</li>
+                            <li>Advertise</li>
+                            <li>Terms of use and Privacy</li>
+                            <li>Ad Choice</li>
+                            <li>Blog</li>
+                            <li>AI</li>
+                        </ul>
+                    </main>
+                    <div className='image'>
+                        <h1>
+                            <img src={home} height='36px' width='36px' alt='logo' />
+                            <span>Zellow</span>
+                            <img src={facebook} height='36px' width='36px' alt='logo' />
+                            <img src={twitter} height='36px' width='36px' alt='logo' />
+                        </h1>
+                        <div></div>
                     </div>
                 </div>
             </div>
