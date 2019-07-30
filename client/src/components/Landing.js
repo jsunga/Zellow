cbde66d94113618923ca84478809839c0a1b442f
jsunga <jawyn.sunga@gmail.com>
2019-07-29 20:34:29 -0700

    finish landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -21,13 +21,34 @@ export default class Landing extends Component {
                         <h1>So you'll never miss a beat.</h1>
                         <div>
                             <section>
-
+                                <h2>Buy a home</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Nullam et lorem dictum magna imperdiet pretium eu nec nisi. 
+                                    Donec vehicula porta erat ut mattis. Nam dapibus lectus tempus 
+                                    eros auctor, et accumsan mi vestibulum.
+                                </p>
+                                <button>Search homes</button>
                             </section>
                             <section>
-                                
+                                <h2>Sell a home</h2>
+                                <p>
+                                    Maecenas consectetur efficitur ex nec euismod. Vivamus porttitor 
+                                    vel quam eget dictum. Aliquam id ultricies metus, nec ullamcorper 
+                                    nisi. Nulla quis accumsan ex. Aliquam eu urna et leo venenatis 
+                                    faucibus vitae sit amet tellus.
+                                </p>
+                                <button>See your options</button>
                             </section>
                             <section>
-                                
+                                <h2>Rent a home</h2>
+                                <p>
+                                    Nam imperdiet aliquam ligula at interdum. Aenean sed purus volutpat, 
+                                    malesuada orci eget, fringilla sapien. Aliquam dolor orci, accumsan 
+                                    at imperdiet nec, molestie id leo. Donec maximus sem et mi posuere.
+                                    Curabitur vel nisi magna.
+                                </p>
+                                <button>Find rentals</button>
                             </section>
                         </div>
                     </div>
