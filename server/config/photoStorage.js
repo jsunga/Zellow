e5120f45c93c14d5328f7dd5b8bcc5f686fc18bb
jsunga <jawyn.sunga@gmail.com>
2019-08-03 14:20:51 -0700

    add api endpoints, migrations, and seeders

--- server/config/photoStorage.js
+++ server/config/photoStorage.js
@@ -3,13 +3,14 @@ const crypto = require('crypto')
 const path = require('path')
 
 const storage = multer.diskStorage({
-    destination: './api/assets',
-    filename: (req, file, callback) => {
-        crypto.pseudoRandomBytes(16, (err, raw) => {
-            if(err) return callback(err)
-            callback(null, raw.toString('hex') + path.extname(file.originalname))
-        })
-    }
+  destination: './api/assets',
+  filename: (req, file, callback) => {
+    crypto.pseudoRandomBytes(16, (err, raw) => {
+      if(err) return callback(err)
+
+      callback(null, raw.toString('hex') + path.extname(file.originalname))
+    })
+  }
 })
 
 const upload = multer({storage: storage})
--- server/config/session.js
+++ server/config/session.js
@@ -4,11 +4,13 @@ const PgSession = require('connect-pg-simple')(session)
 const { connection } = require('../api/db')
 
 const sessionMiddleware = session({
-    store: new PgSession({ conString: connection }),
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }
+  store: new PgSession({
+    conString: connection
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }
 })
 
 module.exports = sessionMiddleware
