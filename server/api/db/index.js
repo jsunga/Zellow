fb1ef16b1eab285497f2095cfda07428cdf9291d
jsunga <jawyn.sunga@gmail.com>
2019-08-04 22:49:22 -0700

    add api endpoints

--- server/api/db/index.js
+++ server/api/db/index.js
@@ -3,10 +3,10 @@ const pgp = require('pg-promise')()
 let connection = ''
 
 if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config()
-  connection = process.env.LOCAL_DATABASE_URL
+    require('dotenv').config()
+    connection = process.env.LOCAL_DATABASE_URL
 } else {
-  connection = process.env.DATABASE_URL
+    connection = process.env.DATABASE_URL
 }
 
 const db = pgp(connection)
