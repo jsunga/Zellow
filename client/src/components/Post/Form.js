cf81d8e57250294573c5763fd2df9570889c3b27
jsunga <jawyn.sunga@gmail.com>
2019-09-04 23:03:26 -0700

    update post forms

--- client/src/components/Post/Form.js
+++ client/src/components/Post/Form.js
@@ -37,33 +37,37 @@ export default class Form extends Component {
                     <input placeholder='Price' />
                     <select>
                         <option value="">Property Type</option>
-                        <option value="San Francisco">San Francisco</option>
-                        <option value="Daly City">Daly City</option>
-                        <option value="San Bruno">San Bruno</option>
-                        <option value="South City">South City</option>
-                        <option value="Colma">Colma</option>
-                        <option value="Oakland">Oakland</option>
+                        <option value="Apartment">Apartment</option>
+                        <option value="House">House</option>
+                        <option value="Condo">Condo</option>
+                        <option value="Townhome">Townhome</option>
                     </select>
                     <select>
                         <option value="">Number of Bedrooms</option>
-                        <option value="San Francisco">San Francisco</option>
-                        <option value="Daly City">Daly City</option>
-                        <option value="San Bruno">San Bruno</option>
-                        <option value="South City">South City</option>
-                        <option value="Colma">Colma</option>
-                        <option value="Oakland">Oakland</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
                     </select>
                     <select>
                         <option value="">Number of Bathrooms</option>
-                        <option value="San Francisco">San Francisco</option>
-                        <option value="Daly City">Daly City</option>
-                        <option value="San Bruno">San Bruno</option>
-                        <option value="South City">South City</option>
-                        <option value="Colma">Colma</option>
-                        <option value="Oakland">Oakland</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
                     </select>
                     <input placeholder='Squarefoot' />
+                    <input placeholder='Tags ex. garage, kitchen, pets' />
+                    <label>Upload Images</label>
+                    <input type='file' name='file' />
                 </section>
+                <section>
+                    <h2>Description</h2>
+                    <textarea />
+                </section>
+                <button>Submit</button>
             </form>
             </>
         )
