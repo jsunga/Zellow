da8085cc1465b31cad3d9a8777179bbff5173d25
jsunga <jawyn.sunga@gmail.com>
2019-09-09 15:29:22 -0700

    add aws settings

--- server/api/index.js
+++ server/api/index.js
@@ -8,7 +8,9 @@ const sessionMiddleware = require('../config/session')
 
 const port = process.env.PORT || 5000
 const app = express()
+const path = require('path')
 
+app.use(express.static(path.join(__dirname, '../../client/build')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(sessionMiddleware)
@@ -18,5 +20,13 @@ app.use(passport.session())
 app.use('/api/assets', express.static('api/assets'))
 
 app.use('/api', endpoints)
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'), err => {
+        if (err) {
+            res.status(500).send(err)
+        }
+    })
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
